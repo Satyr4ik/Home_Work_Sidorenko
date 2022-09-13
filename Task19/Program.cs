d1726ad52a7b1cdff490fa8e5333711dca992a45
Satyr <Daranmic@gmail.com>
2022-09-13 18:16:55 +0700

    третье дз

---
+++ Task19/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] number = new int[4];
+int i = 0;
+if (num > 9999 & num < 100000)
+{
+    while (num > 9999 & num < 100000)
+    {
+        if (num > 1000)
+        {
+            int kesh = num % 10;
+            number[i] = kesh;
+            num = num / 10;
+            i++;
+        }
+        else if (num > 99)
+        {
+            num = num / 10;
+        }
+        else
+        {
+            int kesh = num % 10;
+            number[i] = kesh;
+            num = num / 10;
+            i++;
+        }
+    }
+
+
+    if (number[0] == number[3] & number[1] == number[2])
+    {
+        Console.WriteLine("Данное число являеться палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine("Данное число не являеться палиндромом.");
+    }
+}
+else
+{
+    Console.WriteLine("Введите пятизначное число.");
+}
+
+
---
+++ Task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] kub = new int[num];
+int i = 0;
+
+while (i <= (num - 1))
+{
+    kub[i] = i + 1;
+    i++;
+}
+i = 0;
+Console.Write($"{num} -> ");
+while (i <=(num - 1))
+{
+    int result = Convert.ToInt32(Math.Pow(kub[i], 3));
+    Console.Write($" {result}, ");
+    i++;
+}
