ad172cf21e6175616610645e6e4d70f93ca50459
Satyr4ik <110711698+Satyr4ik@users.noreply.github.com>
2022-09-16 19:13:35 +0700

    Update Program.cs

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,21 +2,31 @@
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
 Console.Clear();
+int[] CreateArrayRndInt(int size)
+{
 int count = 0;
-int[] arr = new int[8];
+int[] array = new int[size];
 
-while(count < 8)
+while(count < size)
 {
 Console.Write($"Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-arr[count] = num;
+array[count] = num;
 count ++;
 }
-count = 0;
-Console.Write("[");
-while(count < 8)
+ return array;
+}
+void PrintArray(int[] array)
 {
-Console.Write($"{arr[count]},");
-count ++;
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
 }
-Console.Write("]");
+
+int[] arr = CreateArrayRndInt(8);
+PrintArray(arr);
