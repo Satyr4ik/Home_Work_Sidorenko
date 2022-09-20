1ceb9d4944607d0bba0412ad1600c67db74d7f35
Satyr <Daranmic@gmail.com>
2022-09-20 15:35:43 +0700

    шестое дз

---
+++ Task41/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите элемент массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int AmountNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.Clear();
+int[] arr = CreateArray(5);
+PrintArray(arr);
+int count = AmountNum(arr);
+Console.WriteLine($"->{count}");
---
+++ Task43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Intersection(double k1, double b1, double k2, double b2)
+{
+    double intersX = 0.0;
+    double intersY = 0.0;
+    intersX = (b2 - b1) / (k1 - k2);
+    intersY = k2 * intersX + b2;
+    Console.Write($"Пересечение двух прямых: ({intersX}:{intersY})");
+}
+
+Console.Clear();
+Console.WriteLine("Нахождения пересечения двух прямых, заданных уравнениями:");
+Console.WriteLine("y = k1 * x + b1");
+Console.WriteLine("y = k2 * x + b2");
+Console.Write($"Введите значение k1: ");
+double coefficient1 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Введите значение b1: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Введите значение k2: ");
+double coefficient2 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Введите значение b2: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Intersection(coefficient1, num1, coefficient2, num2);
