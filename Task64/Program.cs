9eff4b135f766df73f7b5a3eca11632e69f8ddd2
Satyr <Daranmic@gmail.com>
2022-10-04 16:02:49 +0700

    final

---
+++ Task64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int Natural (int n)
+{
+    if (n > 1) 
+    {
+        Console.Write($"{n}, ");
+        return Natural(n-1);
+    }
+    else
+    { 
+        Console.Write("1");
+        return 1;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Natural(n);
---
+++ Task66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Natural (int begin, int end)
+{
+    int sum =0;
+    Console.WriteLine($"{begin} {end}");
+    while (begin <= end)
+    {
+        if (begin >= 1) 
+        {
+            sum = sum + begin;
+            begin++;
+        }
+        else begin++;
+    }
+
+    return sum;
+}
+
+Console.Clear();
+
+
+Console.Write("Введите начало промежутка натурального ряда: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конец промежутка натурального ряда: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa = Natural (m,n);
+
+Console.WriteLine($"Сумма натуральных чисел равна: {summa}");
